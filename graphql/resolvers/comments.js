abbd748e655f6ff38ad17a17828666721496b92b
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-10-25 11:57:40 +0800

    init

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,64 @@
+const Post = require("../../models/Post");
+const { AuthenticationError, UserInputError } = require("apollo-server");
+const checkAuth = require("../../util/check-auth");
+
+module.exports = {
+  Mutation: {
+    createComment: async (_, { postId, body }, context) => {
+      const { username } = checkAuth(context);
+      if (body.trim() === "") {
+        throw new UserInputError("Empty comment", {
+          errors: {
+            body: "Comment must NOT be epty",
+          },
+        });
+      }
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        post.comments.unshift({
+          body,
+          username,
+          createdAt: new Date().toISOString(),
+        });
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post Not found");
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //if already liked, unlike
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("post not found");
+    },
+  },
+};
---
+++ graphql/resolvers/posts.js
@@ -0,0 +1,66 @@
+const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
+
+const { AuthenticationError } = require("apollo-server");
+module.exports = {
+  Mutation: {
+    async createPost(_, { body, title }, context) {
+      const user = checkAuth(context);
+      if (body.trim() === "") {
+        throw new Error("Post body must not be empty");
+      }
+      if (title.trim() === "") {
+        throw new Error("Post title must not be empty");
+      }
+      console.log(user);
+      const newPost = new Post({
+        body,
+        title,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = await newPost.save();
+
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find().sort({ createdAt: -1 });
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
