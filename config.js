6e8f7f3c7b6e24b99c789cb18a3f401edacf788e
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-10-29 13:37:00 +0800

    Change password config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGODB:
-    "mongodb+srv://genelorenz:5VqEMrBpFdJH8XX7@cluster0.grjtv.mongodb.net/merng?retryWrites=true&w=majority",
+    "mongodb+srv://genelorenz:n4xZIHdfzH6bnC0Y@cluster0.grjtv.mongodb.net/merng?retryWrites=true&w=majority",
   SECRET_KEY:
     "FFSDFWHsF-0009888FUjF0uf0-ufUFwyu809/f.OP,uy809f8hgISIbyuSgbsBSUYIsuVUD9AYHFAW9/YFHR9wy09YHFfyh89WYFH8,./sfUF0ujf0UYF0WfhFfr8/,9wyh8awq9tgff8w9YF8AEFA89TG89G89GFV9E8WF./W998WEY9EW8EQ9",
 };
