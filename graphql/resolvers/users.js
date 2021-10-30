851c9784000a4585976291d32e7f18e7089e3a19
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-10-30 20:00:29 +0800

    modified users.js

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -184,7 +184,7 @@ module.exports = {
   Query: {
     async getUsers() {
       try {
-        const users = await User.find().sort({ createdAt: -1 });
+        const users = await User.find();
         return users;
       } catch (err) {
         throw new Error(err);
